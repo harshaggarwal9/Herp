0692ae63691c6b4c97630254fe29aa3c2b140f1b
mohitJoshi0527 <mohit.mjoshi2005@gmail.com>
2025-05-28 13:25:00 +0530

    final try

--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { authMiddleware } from "../middleware/authMiddleware";
-import { roleMiddleware } from "../middleware/roleMiddleware";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { roleMiddleware } from "../middleware/roleMiddleware.js";
 import { getNotification, postNotification } from "../controllers/notification.controller.js";
 
 const router = express.Router();
-router.post("/create",authMiddleware,roleMiddleware['admin'],postNotification);
+router.post("/create",authMiddleware,roleMiddleware(['admin']),postNotification);
 router.get("/get",authMiddleware,getNotification);
 export default router;
