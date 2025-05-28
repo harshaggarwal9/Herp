3064ab4fd684516633b12279e750259a463dd8b0
mohitJoshi0527 <mohit.mjoshi2005@gmail.com>
2025-05-28 13:11:53 +0530

    resolving errors

--- frontend/src/components/getNotifcations.jsx
+++ frontend/src/components/getNotifcations.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { toast } from 'react-toastify';
+import { toast } from 'react-hot-toast';
 import { Bell } from 'lucide-react';
 
 export default function GetNotifications() {
